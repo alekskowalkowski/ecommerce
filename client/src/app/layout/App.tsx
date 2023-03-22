baf4f3aca218ce2198ba268dc3163879e3955cf6
Aleks Kowalkowski <alekskowalkowski@gmail.com>
2023-03-22 11:00:34 -0400

    End of Sections 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import Catalog from "../../features/catalog/Catalog";
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 
 function App() {
@@ -26,7 +27,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} onChange={handleChange}/>
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
       
     </ThemeProvider>
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,18 +1,57 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography, List, ListItem, IconButton, Badge, Box } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+
+import { NavLink } from "react-router-dom";
 
 interface Props {
     darkMode: boolean;
     onChange: () => void;
 }
 
+const midLinks = [
+    { title: 'catalog', path: '/catalog' },
+    { title: 'about', path: '/about' },
+    { title: 'contact', path: '/contact' },
+];
+
+const rightLinks = [
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' },
+];
+
 export default function Header({ darkMode, onChange }: Props) {
     return (
         <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    Ecommerce
-                </Typography>
-                <Switch onChange={onChange} checked={darkMode}/>
+            <Toolbar sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Typography variant="h6" component={NavLink} to="/" sx={{ color: "inherit", textDecoration: "none" }}>
+                        Ecommerce
+                    </Typography>
+                    <Switch onChange={onChange} checked={darkMode}/>
+                </Box>
+                <Box>
+                    <List sx={{ display: "flex" }}>
+                        {midLinks.map(({ title, path}) => (
+                            <ListItem component={NavLink} to={path} key={path} sx={{ color: "inherit", typography: "h6", "&:hover": {color: "grey.500"}, "&.active": {color: "text.secondary"}}}>
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <IconButton size="large" edge="start" color="inherit" sx={{mr: 2}}>
+                        <Badge badgeContent="4" color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: "flex" }}>
+                        {rightLinks.map(({ title, path}) => (
+                            <ListItem component={NavLink} to={path} key={path} sx={{ color: "inherit", typography: "h6"}}>
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     );
